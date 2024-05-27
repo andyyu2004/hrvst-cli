3d77fd01e9d71bd73f69af1846378608544c32c7
Andy Yu <andyyu2004@gmail.com>
2024-05-27 14:48:14 +1200

    specify date for logs

--- src/commands/log-time.ts
+++ src/commands/log-time.ts
@@ -7,67 +7,73 @@ import { urlArgOptions } from "../utils/postman-request-command";
 import { handler as startTimerHandler } from "./start-timer";
 
 type LogTimeArguments = Arguments & {
-  alias?: string;
-  hours: number;
-  notes?: string;
-  project_id?: number;
-  task_id?: number;
+    alias?: string;
+    date: string;
+    hours: number;
+    notes?: string;
+    project_id?: number;
+    task_id?: number;
 };
 
-export const command = "log <hours> [alias]";
+export const command = "log <date> <hours> [alias]";
 
 export const describe = "Create a time entry";
 
 export const builder: CommandBuilder = (yargs) => {
-  const defaultOptions = urlArgOptions(new postman.Url(request.url));
-  const options: Record<string, Options> = _.pick(
-    defaultOptions,
-    "notes",
-    "project_id",
-    "task_id"
-  );
-  Object.values(options).forEach((o) => (o.demandOption = false));
-  options["editor"] = {
-    alias: "e",
-    description: "Launch editor to add notes",
-    type: "boolean",
-  };
-  return yargs
-    .positional("hours", {
-      describe: "Amount of hours to log",
-      type: "number",
-    })
-    .positional("alias", {
-      describe: "Alias for project id and task id",
-    })
-    .options(options)
-    .version(false);
+    const defaultOptions = urlArgOptions(new postman.Url(request.url));
+    const options: Record<string, Options> = _.pick(
+        defaultOptions,
+        "notes",
+        "project_id",
+        "task_id"
+    );
+    Object.values(options).forEach((o) => (o.demandOption = false));
+    options["editor"] = {
+        alias: "e",
+        description: "Launch editor to add notes",
+        type: "boolean",
+    };
+    return yargs
+        .positional("date", {
+            describe: "The date on which the time entry was spent",
+            type: "string",
+        })
+        .positional("hours", {
+            describe: "Amount of hours to log",
+            type: "number",
+        })
+        .positional("alias", {
+            describe: "Alias for project id and task id",
+        })
+        .options(options)
+        .version(false);
 };
 
 export const handler = async (args: LogTimeArguments): Promise<void> => {
-  if (isNaN(args.hours)) {
-    console.error(chalk.red("Hours must be a valid number"));
-    return;
-  }
+    if (isNaN(args.hours)) {
+        console.error(chalk.red("Hours must be a valid number"));
+        return;
+    }
 
-  const formatTime = (date: Date) =>
-    date
-      .toLocaleString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      })
-      .replace(" ", "")
-      .toLowerCase();
+    const formatTime = (date: Date) =>
+        date
+            .toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+            })
+            .replace(" ", "")
+            .toLowerCase();
 
-  const hoursInSeconds = args.hours * 60 * 60;
-  const date = new Date();
-  date.setSeconds(date.getSeconds() - hoursInSeconds);
+    const hoursInSeconds = args.hours * 60 * 60;
+    const date = new Date();
+    date.setSeconds(date.getSeconds() - hoursInSeconds);
 
-  await startTimerHandler({
-    fields: "client.name,id,hours,notes,project.name,spent_date,task.name",
-    started_time: formatTime(date),
-    ended_time: formatTime(new Date()),
-    ...args,
-  });
+    await startTimerHandler({
+        fields: "client.name,id,hours,notes,project.name,spent_date,task.name",
+        started_time: formatTime(date),
+        ended_time: formatTime(new Date()),
+        spent_date: args.date,
+        ...args,
+    });
 };
--- src/commands/start-timer.ts
+++ src/commands/start-timer.ts
@@ -3,23 +3,23 @@ import _ from "lodash";
 import postman from "postman-collection";
 import { Arguments, CommandBuilder, Options } from "yargs";
 import {
-  request,
-  handler as defaultHandler,
+    request,
+    handler as defaultHandler,
 } from "../generated-commands/time-entries/create";
 import { Alias, getConfig } from "../utils/config";
 import { urlArgOptions } from "../utils/postman-request-command";
 import {
-  getCurrentLocalISOString,
-  getNotes,
-  normalizeProjectAndTaskAssignment,
+    getCurrentLocalISOString,
+    getNotes,
+    normalizeProjectAndTaskAssignment,
 } from "../utils/timer";
 
 type StartTimerArguments = Arguments & {
-  alias?: string;
-  editor?: boolean;
-  notes?: string;
-  project_id?: number;
-  task_id?: number;
+    alias?: string;
+    editor?: boolean;
+    notes?: string;
+    project_id?: number;
+    task_id?: number;
 };
 
 export const command = "start [alias]";
@@ -27,54 +27,54 @@ export const command = "start [alias]";
 export const describe = "Create a running time entry";
 
 export const builder: CommandBuilder = (yargs) => {
-  const defaultOptions = urlArgOptions(new postman.Url(request.url));
-  const options: Record<string, Options> = _.pick(
-    defaultOptions,
-    "notes",
-    "project_id",
-    "task_id"
-  );
-  Object.values(options).forEach((o) => (o.demandOption = false));
-  options["editor"] = {
-    alias: "e",
-    description: "Launch editor to add notes",
-    type: "boolean",
-  };
-  return yargs
-    .positional("alias", {
-      describe: "Alias for project id and task id",
-    })
-    .options(options)
-    .version(false);
+    const defaultOptions = urlArgOptions(new postman.Url(request.url));
+    const options: Record<string, Options> = _.pick(
+        defaultOptions,
+        "notes",
+        "project_id",
+        "task_id"
+    );
+    Object.values(options).forEach((o) => (o.demandOption = false));
+    options["editor"] = {
+        alias: "e",
+        description: "Launch editor to add notes",
+        type: "boolean",
+    };
+    return yargs
+        .positional("alias", {
+            describe: "Alias for project id and task id",
+        })
+        .options(options)
+        .version(false);
 };
 
 export const handler = async (args: StartTimerArguments): Promise<void> => {
-  const defaultArgs = {
-    fields: "client.name,id,is_running,notes,project.name,spent_date,task.name",
-    spent_date: getCurrentLocalISOString(),
-  };
+    const defaultArgs = {
+        fields: "client.name,id,is_running,notes,project.name,spent_date,task.name",
+        spent_date: args.spent_date,
+    };
 
-  if (args.alias?.length) {
-    const config = await getConfig();
-    const alias: Alias = _.get(
-      config,
-      `accountConfig.${config.accountId}.aliases.${args.alias}`
-    );
-    if (!alias) {
-      console.error(chalk.yellow("Alias not found"));
-      return;
+    if (args.alias?.length) {
+        const config = await getConfig();
+        const alias: Alias = _.get(
+            config,
+            `accountConfig.${config.accountId}.aliases.${args.alias}`
+        );
+        if (!alias) {
+            console.error(chalk.yellow("Alias not found"));
+            return;
+        }
+        await defaultHandler(
+            Object.assign(defaultArgs, args, {
+                notes: await getNotes(args),
+                project_id: alias.projectId,
+                task_id: alias.taskId,
+            })
+        );
+    } else {
+        const demandedArgs = await normalizeProjectAndTaskAssignment(args);
+        await defaultHandler(
+            Object.assign(defaultArgs, demandedArgs, { notes: await getNotes(args) })
+        );
     }
-    await defaultHandler(
-      Object.assign(defaultArgs, args, {
-        notes: await getNotes(args),
-        project_id: alias.projectId,
-        task_id: alias.taskId,
-      })
-    );
-  } else {
-    const demandedArgs = await normalizeProjectAndTaskAssignment(args);
-    await defaultHandler(
-      Object.assign(defaultArgs, demandedArgs, { notes: await getNotes(args) })
-    );
-  }
 };
